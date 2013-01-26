d01aad09bea13ee49a3307e802ebc412562c7480
thk123 <thekileyenator@gmail.com>
2013-01-26 01:38:32 +0000

    Added grid with method to get closest grid square from a point

---
+++ Codebase/Grid.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace GGJ_DisasterMode.Codebase
+{
+    class Grid
+    {
+        Rectangle gridRectangle;
+        float gridSize;
+
+        public Grid(Rectangle drawRectangle, float individualGridSize)
+        {
+            gridRectangle = drawRectangle;
+            gridSize = individualGridSize;
+        }
+
+        public Point GetGridPointFromMousePosition(Point mousePosition)
+        {
+            if (gridRectangle.Contains(mousePosition))
+            {
+                Point gridPoint = new Point();
+                gridPoint.X = mousePosition.X - gridRectangle.X;
+                gridPoint.Y = mousePosition.Y - gridRectangle.Y;
+
+                double xPos = gridPoint.X / (float)gridSize;
+                double yPos = gridPoint.Y / (float)gridSize;
+
+                //Round the floating values
+                xPos = Math.Floor(xPos + 0.5f);
+                yPos = Math.Floor(yPos + 0.5f);
+
+                gridPoint.X = (int)xPos;
+                gridPoint.Y = (int)yPos;
+
+                return gridPoint;
+            }
+            else
+            {
+                //invalid point, outside the grid
+                return new Point(-1, -1);
+            }
+        }
+
+    }
+}
